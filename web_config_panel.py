80a3b908f2d2c29db8185a84028267f24cfdc3a9
Yongning Liang <liangyongning@gmail.com>
2014-10-07 16:59:35 +0800

    http server

--- web_config_panel.py
+++ web_config_panel.py
@@ -1,5 +1,8 @@
 import sys
 
+# version of web config panel
+__VERSION__ = '0.0.1'
+
 # detect python's version
 python_version = sys.version_info[0]
 
@@ -19,4 +22,23 @@ class WebConfigPanel(BaseHTTPRequestHandler):
 
     def version_string(self):
         '''overwrite HTTP server's version string'''
-        return ''
+        return 'WebConfigPanel/%s Sublime/%s' % (__VERSION__, sublime.version())
+
+    def do_GET(self):
+        '''Serve a GET request.'''
+        f = self.send_head()
+        if f:
+            try:
+                self.copyfile(f, self.wfile)
+            finally:
+                f.close()
+
+    def do_HEAD(self):
+        '''Serve a HEAD request.'''
+        f = self.send_head()
+        if f:
+            f.close()
+
+    def send_head(self):
+        '''Send header to the client'''
+        pass
